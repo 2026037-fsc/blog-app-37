886f83273674fc25caa81e34a2edd62cdbbd4c6c
Sakamoto <2026037@fsc-educ.com>
2026-06-04 15:02:25 +0900

    コントローラー変更

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -2,13 +2,13 @@ package com.example.blog_app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 public class BlogController {
 
-    @GetMapping("/blogs")
+    @GetMapping("/")
     public String blogs() {
         return "blogs";
     }
